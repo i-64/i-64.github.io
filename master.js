b17357496595927792a245eb030d4a1e4cf7d8d2
Mrunal Ahire <mrunal.ahire@gmail.com>
2020-12-13 16:21:05 +0530

    ui enhancements

---
+++ master.js
@@ -0,0 +1,18 @@
+function script () {
+    for (var i = 1; i <= 9; i++) {
+        // console.log(i)
+        const _i = i
+        document.getElementById('' + _i).onmouseover = function () {
+            document.getElementById('preview' + _i).style.display = 'block'
+            document.getElementById('preview-default').style.display = 'none'
+            // $('#preview-default').hide();
+            // $('#preview' + _i).show(200);
+        }
+        document.getElementById('' + _i).onmouseout = function () {
+            document.getElementById('preview' + _i).style.display = 'none'
+            document.getElementById('preview-default').style.display = 'block'
+            // $('#preview' + _i).hide()
+            // $('#preview-default').show(200)
+        }
+    }
+}
